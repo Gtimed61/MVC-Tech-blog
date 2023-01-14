2db4604d12ef92d0a7147d7a353338bc97afabaa
Grant Thorne <gthorne61@gmail.com>
2023-01-14 17:30:18 -0600

    models/utils/server

--- models/comment.js
+++ models/comment.js
@@ -3,5 +3,6 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init(
-    
+
+
+module.exports = Comment;
---
+++ models/post.js
@@ -0,0 +1,10 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create our Post model
+
+
+//create fields/columns for Post model
+
+
+module.exports = Post;
